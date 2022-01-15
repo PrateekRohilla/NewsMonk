b5a4d8c119926bfcce7a277a36515e9983600100
prateekrohilla <prateek98rohilla@gmail.com>
2022-01-15 15:17:58 +0530

    top loading bar added

--- src/components/News.js
+++ src/components/News.js
@@ -34,15 +34,19 @@ export class News extends Component {
   }
 
   async updateNews(){
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=73a3b63e516d442e8c5daa55c8f7b5d2&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles, 
       totalResults: parsedData.totalResults, 
       loading: false
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount(){
